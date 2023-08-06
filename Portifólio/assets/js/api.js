57bb0f2e15f04a502afa5e2593a086b0e8fbf25f
Elberth Junior <elberthasj@hotmail.com>
2023-08-06 19:03:50 -0300

    Raw

--- Portifólio/assets/js/api.js
+++ Portifólio/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/elberthjr/portifolio/blob/main/Portifólio/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/elberthjr/portifolio/main/Portifólio/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
